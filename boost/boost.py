fab88d02ad666b68e7714de91ccd7b459dada014
Deleted <60399731+6days9weeks@users.noreply.github.com>
2020-08-08 01:27:07 +0900

    Update boost.py

--- boost/boost.py
+++ boost/boost.py
@@ -3,14 +3,15 @@ from discord.ext import commands
 class Boost(commands.Cog):
     def __init__ (self, bot):
         self.bot = bot
-
+        self.log_channel = 701683911872479252
     @commands.Cog.listener()
     async def on_message(self, message):
 
         print(message.type)
         if message.type == discord.MessageType.premium_guild_subscription:
-            embed = discord.Embed(title=f"**Nitro Boost**", description=f"Thank you so much for boosting <a:BoostingAnimated:717651091260702751> !", color=0xff0000)
-            m = await message.channel.send(embed=embed)
+            log_channel = guild.get_channel(self.log_channel)
+         embed = discord.Embed(title=f"**Nitro Boost**", description=f"Thank you so much for boosting <a:BoostingAnimated:717651091260702751> !", color=0xff0000)
+            m = await log_channel.send(embed=embed)
             await m.add_reaction("<a:Heartanimated:717651074126708756>")
             
 def setup(bot):
