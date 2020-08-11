300da6fb7b5013feea39359c586ee09439a2400e
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-08-11 16:37:25 +0900

    Update uptime.py

--- uptime/uptime.py
+++ uptime/uptime.py
@@ -8,18 +8,18 @@ from core.models import PermissionLevel
 from core.decorators import trigger_typing
 
 
-class Howto(commands.Cog):
+class Uptime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     @checks.has_permissions(PermissionLevel.REGULAR)
     @trigger_typing
-    async def howto(self, ctx):
-        """Help command in brief"""
+    async def uptime(self, ctx):
+        """Shows uptime for the bot."""
 
-        desc = "Just an easy explanation on how the bot works"
-        desc += "this is just the tip of the iceberg this bot can do much moreee."
+        desc = "This is a command to view how long your bot has been "
+        desc += "online since the last time it restarted."
 
         embed = discord.Embed(
             description=desc,
@@ -28,13 +28,13 @@ class Howto(commands.Cog):
         )
 
         embed.set_author(
-            name="How To Use Nezubabey",
+            name="Bot - Uptime",
             icon_url=self.bot.user.avatar_url,
         )
 
         embed.set_thumbnail(url=self.bot.user.avatar_url)
 
-        embed.add_field(name="Explanation", value= "**The commands are explained as followed -**\n**To reply normally:** `nezur` or @nezubabey#2849 r,\n**To reply anonymously:** `nezuar` or @nezubabey#2849 ar,\n**To close a thread:** to close without any name or who did it just type `nezuclose silenty ~~reason~~` to close after sometime do `nezuclose in 5m etc` ,\n**To block a certain person from dming the bot:** `nezublock userid or usermention`,\n**To get the loglink of this thread:** `nezuloglink`,\n**To check logs of user:** `nezulogs user`,\n**To make the bot say something:** `nezusay` only for owner,\n**To delete a message:** `nezudelete messageid`,\n**To open a thread with a person without them dming the bot:** `nezucontact userid or mention`,\n**To get pinged if user replies in thread:** `nezusubscribe`,\n**To add an tag:** `nezutags add "tag name" value`,\n\nAny questions? Just Dm or ask me my tag is `chizuru#0001` @chizuru#0001")
+        embed.add_field(name="Uptime", value=self.bot.uptime)
 
         await ctx.send(embed=embed)
 
