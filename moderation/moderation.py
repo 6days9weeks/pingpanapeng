6394cc83206b13247077cbbec55b0560dab118ee
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-08-14 01:20:03 +0900

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -480,5 +480,21 @@ class moderation(commands.Cog):
             )
             await ctx.send(embed = embed)
 
+@commands.command()
+async def afk(ctx, mins):
+    current_nick = ctx.author.nick
+    await ctx.send(f"{ctx.author.mention} has gone afk for {mins} minutes.")
+    await ctx.author.edit(nick=f"{ctx.author.name} [AFK]")
+
+    counter = 0
+    while counter <= int(mins):
+        counter += 1
+        await asyncio.sleep(60)
+
+        if counter == int(mins):
+            await ctx.author.edit(nick=current_nick)
+            await ctx.send(f"{ctx.author.mention} is no longer AFK")
+            break
+
 def setup(bot):
     bot.add_cog(moderation(bot))
