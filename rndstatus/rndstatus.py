d764047aa130d793a1d9f45c8349bfbb33073c7c
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-01 06:55:51 +0900

    Update rndstatus.py

--- rndstatus/rndstatus.py
+++ rndstatus/rndstatus.py
@@ -32,8 +32,8 @@ class CustomStatus(commands.Cog):
 
     @status_group.command(name="start")
     async def statusy_start(self, ctx):
-        if self.first == None or self.second == None or self.third == None:
-            await ctx.send("Please set the 3 Status's first!")
+        if self.first == None or self.second == None or self.third == None or self.fourth == None:
+            await ctx.send("Please set the 4 Status's first!")
         else:
             self.start_the_status.start()
             await ctx.send("Done! If you experience any problems just run this command again!")
