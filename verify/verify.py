71c90abd123e3c85f6280a30969fba157c72b38a
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-04 02:22:46 +0900

    Sugma

--- verify/verify.py
+++ verify/verify.py
@@ -9,16 +9,16 @@ class Verify(commands.Cog):
         self.bot = bot
         self.maincolor = 0x06c9ff
         self.errorcolor = 0xFF0000
-        self.log_channel = 708197652545667072  # ID of the log channel
-        self.defaultRole = 705764532303167568  # ID of the Discord Member role
-       
+        self.log_channel = 751101899603705988  # ID of the log channel
+        self.defaultRole = 751119227993456703  # ID of the Discord Member role
+        self.unverifiedRole = 751096857324945500 # ID of unverified role
 
     @commands.command(name="send-verify")
     @commands.has_permissions(administrator=True)
     async def sendverifymsg(self, ctx):
         embed = discord.Embed(
             title="**Verification**",
-            description="To gain access to Chiasa Discord Server, you will have to verify.\n\nVerifying means that you have agreed to our Rules and Guidelines\n\nOur Rules and Guidelines can be found in <#711667049465905302> and for info on our server check <#711667131820933210>!\n\nTo verify, please type `verify` in this channel!",
+            description="To gain access to Dasher's Server, you will have to verify.\n\nVerifying means that you have agreed to our Rules.\n\nOur Rules can be found in <#751100129842430023>!\n\nTo verify, please type `read` in this channel!",
             color=self.maincolor
         )
         await ctx.send(embed=embed)
@@ -29,18 +29,18 @@ class Verify(commands.Cog):
             return
 
         else:
-            if message.channel.id == 705764310109913138:
-                if message.content.lower() == "verify":
+            if message.channel.id == 751127767604527156:
+                if message.content.lower() == "read":
                     guild = message.guild
                     role = guild.get_role(self.defaultRole)
-                   
-                   
+                    un = guild.get_role(self.unverifiedRole)
+                    await message.author.remove_roles(un)
                     await message.author.add_roles(role)
                     log_channel = guild.get_channel(self.log_channel)
                     await message.add_reaction("\U00002705")
                     embed = discord.Embed(
                         title="Someone just verified!",
-                        description=f"{message.author.mention} just verified!\n\nTheir ID is {message.author.id}\n\nAccount created at **{message.author.created_at}**.",
+                        description=f"{message.author.mention} just verified!\n\nHis ID is {message.author.id}\n\nThe message ID is {message.id}\nThe channel ID is {message.channel.id}\nThe message was sent at {message.created_at}",
                         color=self.maincolor
                     )
                     await log_channel.send(embed=embed)
@@ -49,7 +49,7 @@ class Verify(commands.Cog):
                     try:
                         embed = discord.Embed(
                             title="Verification",
-                            description="You are now verified in Chiasa Discord Server! Get some roles from <#711673102534181005>",
+                            description="You are now verified in Dasher's Discord Server!",
                             color=self.maincolor
                         )
                         await message.author.send(embed=embed)
