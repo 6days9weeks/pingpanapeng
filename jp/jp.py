cff69950105744cd9e96aa93d3595817f5ade7fe
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-07 03:10:06 +0900

    Create jp.py

---
+++ jp/jp.py
@@ -0,0 +1,30 @@
+import typing
+import datetime
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+from core.paginator import EmbedPaginatorSession
+
+from .resources.bot import JoinResource
+
+
+    class Jp(commands.Cog):
+    """Get join position of a member in your server"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(aliases=["memberinfo", "user", "userinfo"])
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def member(self, ctx, *, member: typing.Union[discord.Member, str] = None):
+        """Get the stats of a member."""
+
+        embed = MemberResource(ctx, member).member_embed()
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Jp(bot))
+
