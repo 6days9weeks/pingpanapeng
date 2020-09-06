7e4c17a77c5417e412a19940e724ff6d5e537364
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-07 03:16:36 +0900

    Update jp.py

--- jp/jp.py
+++ jp/jp.py
@@ -9,14 +9,12 @@ from core.paginator import EmbedPaginatorSession
 
 from .resources.bot import JoinResource
 
-
 class Jp(commands.Cog):
     """Get join position of a member in your server"""
 
     def __init__(self, bot):
         self.bot = bot
-
-
+   
     @commands.command(aliases=["memberinfo", "user", "userinfo"])
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def member(self, ctx, *, member: typing.Union[discord.Member, str] = None):
@@ -27,4 +25,3 @@ class Jp(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Jp(bot))
-
