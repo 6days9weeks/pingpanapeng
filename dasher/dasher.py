47f3d2dca2618c8a613a1f516c00456bbfacc632
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-08 00:10:38 +0900

    Update dasher.py

--- dasher/dasher.py
+++ dasher/dasher.py
@@ -7,17 +7,6 @@ from datetime import datetime
 class Dasher(commands.Cog):
     def __init__(self, bot):
         self.bot = bot              
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot:
-            return
-
-        else:
-            if message.channel.id == 751768597239562260:
-                    await message.add_reaction("\U00002705")
-                    await message.add_reaction("\U0000274C")
-
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
