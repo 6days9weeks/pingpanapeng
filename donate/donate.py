437373649ee77095015c28bb4f01efcac882f82f
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-08 14:04:22 +0900

    Update donate.py

--- donate/donate.py
+++ donate/donate.py
@@ -1,19 +1,22 @@
 import discord
 from discord.ext import commands
+import asyncio
+from datetime import datetime
 
 
 class Donate(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot = bot              
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
 
-if message.channel.id == 692765490615156757:
-            if "donate" in message.content.lower():
-                await message.channel.send(f"hiii")
+        else:
+            if message.channel.id == 751768597239562260:
+                if message.content.lower() == "donate":
+                    await message.channel.send("hiii")
 
 def setup(bot):
     bot.add_cog(Donate(bot))
