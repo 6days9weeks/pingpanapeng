7629dd0660ef60cf1cfba7345a72ef1fcefdf5c4
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-08 13:33:39 +0900

    Create donate.py

---
+++ donate/donate.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+
+class Donate(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+            if "donate" in message.content.lower():
+                await message.channel.send(f"test")
+
+def setup(bot):
+    bot.add_cog(Donate(bot))
