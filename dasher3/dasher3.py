adca6c3cd7d5fd1514b430cf322d57ac0b5973e0
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-09 01:10:08 +0900

    Create dasher3.py

---
+++ dasher3/dasher3.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+import asyncio
+from datetime import datetime
+
+
+class Dasher3(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.maincolor = 0x06c9ff
+        self.errorcolor = 0xFF0000
+        self.defaultRole = 752919582414471349  # ID of the Discord Member role
+       
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        else:
+            if message.channel.id == 752919997621338272:
+                if message.content.lower() == "!role ap":
+                    guild = message.guild
+                    role = guild.get_role(self.defaultRole)
+                   
+                   
+                    await message.author.add_roles(role)
+                   
+                    await message.add_reaction("\U00002705")
+                   
+                    await asyncio.sleep(2)
+                    await ctx.message.delete()
+                    try:
+                        embed = discord.Embed(
+                            title="Role Given",
+                            description="You now have the activity role now go chat",
+                            color=self.maincolor
+                        )
+                        embed.set_thumbnail(url=message.author.avatar_url)
+                        await message.author.send(embed=embed)
+                    except:
+                        print(f"Couldn't send {message.author.name} his verification acceptation")
+
+                else:
+                    await message.delete()
+                    return
+
+
+def setup(bot):
+    bot.add_cog(Dasher3(bot))
