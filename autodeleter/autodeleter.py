ba09f3745e5d65f87538a3d47e15ee9d1dbe2fb0
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-20 16:59:07 +0900

    Create autodeleter.py

---
+++ autodeleter/autodeleter.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import asyncio
+from datetime import datetime
+
+
+class autodeleter(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+       
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        else:
+            if message.channel.id == 757144854265331792:
+               
+                    await message.delete()
+
+
+def setup(bot):
+    bot.add_cog(autodeleter(bot))
