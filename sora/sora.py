42a22284e1b1ec0985df86aadf07d343bdb8cecf
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-21 20:24:22 +0900

    Update sora.py

--- sora/sora.py
+++ sora/sora.py
@@ -9,7 +9,7 @@ class StatusChiasa(commands.Cog):
     @tasks.loop(seconds=10)
     async def start_status(self):
         server = self.bot.get_guild(699158208027164723)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} cuties <3"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} Members!"))
         await asyncio.sleep(10)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=f"Dm for help", url="https://twitch.tv/kawaiii0001"))
         await asyncio.sleep(10)
