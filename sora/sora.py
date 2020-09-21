31388929cdf8c4578db748543dc40801c4f4f2e4
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-09-21 20:14:47 +0900

    Update sora.py

--- sora/sora.py
+++ sora/sora.py
@@ -11,7 +11,7 @@ class StatusChiasa(commands.Cog):
         server = self.bot.get_guild(699158208027164723)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} cuties <3"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity, name+f"Dm For Help", url="https://twitch.tv/kawaiii0001"))
+        await self.bot.change_presence(activity=discord.Activity, name=f"Dm For Help", url="https://twitch.tv/kawaiii0001"))
         await asyncio.sleep(10)
         await self.bot.change_presence(activity=discord.Game(name=f"Dm me to host giveaways"))
         await asyncio.sleep(10)
