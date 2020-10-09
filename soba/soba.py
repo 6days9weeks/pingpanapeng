914e58451cd79c2ce83cd63703c2e15a9f9bdfa4
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-10-09 20:18:36 +0900

    Update soba.py

--- soba/soba.py
+++ soba/soba.py
@@ -9,16 +9,18 @@ class StatusNezuko(commands.Cog):
     @tasks.loop(seconds=10)
     async def start_status(self):
         server = self.bot.get_guild(703455306272735313)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} cuties <3"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} Cuties <3"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=f"type donate to donate", url="https://twitch.tv/kawaiii0001"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=f"Type Donate To Donate", url="https://twitch.tv/kawaiii0001"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"buy paid promos"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"Buy Paid Promos"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"ash is mine ~ itsuki"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"Soba is mine - ឵឵❥sasha"))
         await asyncio.sleep(10)
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"Nezuko Hideout | DM For Help | {server.member_count} Members"))
+        await asyncio.sleep(5)
 
-    @commands.command(name="status_start")
+    @commands.command(name="ss")
     async def start_start_cmd(self, ctx):
         self.start_status.start()
         await ctx.send("Done! Re-run this command if it stops working")
