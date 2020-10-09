3d0bea3c0423a834ec96f10c1008050379f954a4
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-10-09 20:45:18 +0900

    Update sora.py

--- sora/sora.py
+++ sora/sora.py
@@ -11,7 +11,7 @@ class StatusChiasa(commands.Cog):
     @tasks.loop(seconds=10)
     async def start_status(self):
         server = self.bot.get_guild(699158208027164723)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} Members🎃🥀!"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{server.member_count} Members!"))
         await asyncio.sleep(10)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=f"DM For Help/Report", url="https://twitch.tv/kawaiii0001"))
         await asyncio.sleep(10)
@@ -19,7 +19,7 @@ class StatusChiasa(commands.Cog):
         await asyncio.sleep(10)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"❥sasha is a bad coder"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"Chiasa | DM For Help | {server.member_count} Members🎃 🥀"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"Chiasa | DM For Help | {server.member_count} Members"))
         await asyncio.sleep(10)
 
     @commands.command(name="ss")
