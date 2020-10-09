96ffeb308d751a4e6f8ef6c796def3b9b8ebfdcb
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-10-09 22:05:23 +0900

    Update nezukoverify.py

--- nezukoverify/nezukoverify.py
+++ nezukoverify/nezukoverify.py
@@ -50,7 +50,7 @@ class CatFish(commands.Cog):
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     async def selfie(self, ctx, member: discord.Member=None):
         """Assign a role to a member."""      
-        await member.add_roles(self.bot.guild.get_role(760529072655958077)), reason="Added by "+ctx.author.display_name+" ("+ctx.author.name+"#"+ctx.author.discriminator+")")
+        await member.add_roles(self.bot.guild.get_role(760529072655958077)), reason="woooo verified"
         await ctx.send(f"Successfully added the role to {member.name}!")            
             
             
