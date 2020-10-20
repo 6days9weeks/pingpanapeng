23baaf9a188d383d9003b4ff4062b866e3c6656d
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-10-20 20:06:28 +0900

    Update ping.py

--- ping/ping.py
+++ ping/ping.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import asyncio
+import datetime
+from core.decorators import trigger_typing
+import aiohttp
 
 class Ping(commands.Cog):
     def __init__(self, bot):
