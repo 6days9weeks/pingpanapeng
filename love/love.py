d86dd9f216913b2125e2927a52a51b628722d490
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-11-02 21:29:40 +0900

    Update love.py

--- love/love.py
+++ love/love.py
@@ -18,10 +18,6 @@ with open(Path(__file__).parent / "love_matches.json", "r", encoding="utf8") as 
     LOVE_DATA = json.load(file)
     LOVE_DATA = sorted((int(key), value) for key, value in LOVE_DATA.items())
 
-def ship_url(love_percent, who, whom):
-    url2 = "https://api.martinethebot.com/v1"  + "/imagesgen/ship?percent=" + str(love_percent) + "&first_user=" + urllib.parse.urlencode(url=who.avatar_url) + "&second_user=" + urllib.parse.urlencode(url=whom.avatar_url)
-    return url2
-
 
 class LoveCal(commands.Cog):
     """A cog for calculating the love between two people.""" 
@@ -50,7 +46,11 @@ class LoveCal(commands.Cog):
 
         m = hashlib.sha256(who.encode() + whom.encode())
         love_percent = sum(m.digest()) % 101
-
+        
+       def ship_url(love_percent, who, whom):
+           url2 = "https://api.martinethebot.com/v1"  + "/imagesgen/ship?percent=" + str(love_percent) + "&first_user=" + urllib.parse.urlencode(url=who.avatar_url) + "&second_user=" + urllib.parse.urlencode(url=whom.avatar_url)
+           return url2
+    
         index = bisect.bisect(LOVE_DATA, (love_percent,)) - 1
         _, data = LOVE_DATA[index]
 
