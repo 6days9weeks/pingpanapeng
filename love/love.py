e7de42c41b8b8f7ea5255df9f380a385f6fac27d
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-11-02 21:03:34 +0900

    Update love.py

--- love/love.py
+++ love/love.py
@@ -62,7 +62,7 @@ class LoveCal(commands.Cog):
         )
         base_api_url = "https://api.martinethebot.com/v1"
         def ship_url(love_percent, who, whom):
-          url = base_api_url + "/imagesgen/ship?precent=" + str(love_percent) + "&first_user=" + urllib.urlencode(url=who.avatar_url_as(static_format="png")) + "&second_user=" + urllib.urlencode(url=who.avatar_url_as(static_format="png"))
+          url = base_api_url + "/imagesgen/ship?percent=" + str(love_percent) + "&first_user=" + urllib.urlencode(url=who.avatar_url_as(static_format="png")) + "&second_user=" + urllib.urlencode(url=who.avatar_url_as(static_format="png"))
           return url 
         
         embed.set_image(url=ship_url)
