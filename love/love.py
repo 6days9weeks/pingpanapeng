855ab85c3e2ea408982e33443703cc4b0a5bb890
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-11-02 21:44:44 +0900

    Update love.py

--- love/love.py
+++ love/love.py
@@ -32,6 +32,10 @@ class LoveCal(commands.Cog):
         Tells you how much the two love each other.
         This command also accepts users or arbitrary strings as arguments.
         """
+        def ship_url(love_percent, who, whom):
+            url2 = "https://api.martinethebot.com/v1"  + "/imagesgen/ship?percent=" + str(love_percent) + "&first_user=" + urllib.parse.urlencode(url=who.avatar_url) + "&second_user=" + urllib.parse.urlencode(url=whom.avatar_url)
+            return url2
+        
         def normalize(arg: Union[Member, str]) -> str:
             if isinstance(arg, Member):
                 arg = str(arg)
@@ -47,9 +51,6 @@ class LoveCal(commands.Cog):
         m = hashlib.sha256(who.encode() + whom.encode())
         love_percent = sum(m.digest()) % 101
         
-        def ship_url(love_percent, who, whom):
-            url2 = "https://api.martinethebot.com/v1"  + "/imagesgen/ship?percent=" + str(love_percent) + "&first_user=" + urllib.parse.urlencode(url=who.avatar_url) + "&second_user=" + urllib.parse.urlencode(url=whom.avatar_url)
-            return url2
     
         index = bisect.bisect(LOVE_DATA, (love_percent,)) - 1
         _, data = LOVE_DATA[index]
