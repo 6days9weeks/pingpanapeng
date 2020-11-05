060f3ed92164413a74340a9829a78093e61be78b
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-11-05 23:58:49 +0900

    e

---
+++ raw/raw.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from core import checks
+
+
+class EmbedRaw(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    @checks.thread_only()
+    async def raw(self, ctx, message_id: int=None):
+        if message_id is None:
+            return await ctx.send("Please provide a message ID")
+        
+        try:
+            msg = await ctx.fetch_message(message_id)
+        except Exception as e:
+            print(str(e))
+        msgg = msg.embeds[0]
+        await ctx.send(f"```{msgg.description}```")
+        
+
+def setup(bot):
+    bot.add_cog(EmbedRaw(bot))
